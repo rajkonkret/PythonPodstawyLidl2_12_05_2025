989b9439c3703827ddeb3b2596b0b51261576b2c
radoslawjaniak <rajkokret660@gmail.com>
2025-11-12 12:52:08 +0100

    [1 5] add formaty

--- formaty.py
+++ formaty.py
@@ -17,3 +17,32 @@ print("witaj %s, masz teraz %d lat" % (user, wiek))
 print(f"Witaj {user}, masz teraz {wiek} lat.")
 # Witaj Tomek, masz teraz 67 lat.
 print("Witaj {}, masz teraz {} lat.".format(user, wiek))
+
+print("Używamy wersji pythona %i" % 3)  # Używamy wersji pythona 3
+print("Używamy wersji pythona %f" % 3)  # Używamy wersji pythona 3.000000
+# %f - float
+print("Używamy wersji pythona %f" % 3)  # Używamy wersji pythona 3.000000
+print("Używamy wersji pythona %.2f" % 3.9)  # Używamy wersji pythona 3.90
+print("Używamy wersji pythona %.1f" % 3.9)  # Używamy wersji pythona 3.9
+print("Używamy wersji pythona %.0f" % 3.9)  # Używamy wersji pythona 4
+print("Używamy wersji pythona %.f" % 3.9)  # Używamy wersji pythona 4
+
+print(f"Uzywamy pythona {wersja}")  # Uzywamy pythona 3.900001
+print(f"Uzywamy pythona {wersja:.2f}")  # Uzywamy pythona 3.90
+print(f"Uzywamy pythona {wersja:.1f}")  # Uzywamy pythona 3.9
+print(f"Uzywamy pythona {wersja:.0f}")  # Uzywamy pythona 4
+
+user = "Tomek"
+print(f"{user:>10}")  # "     Tomek"
+print(f"{user:<20}")  # "Tomek               "
+print(f"{user:^20}")  # "       Tomek        "
+print(liczby)
+print(f"Nasza duza liczba: {liczby:,}")  # Nasza duza liczba: 890,123,456,123
+print(f"Nasza duza liczba: {liczby:_}")  # Nasza duza liczba: 890_123_456_123
+print(f"Nasza duza liczba: {liczby:_}".replace("_", "."))  # Nasza duza liczba: 890.123.456.123
+print(f"Nasza duza liczba: {liczby:_}".replace("_", " "))  # Nasza duza liczba: 890 123 456 123
+
+# liczba = 15000000000000
+liczba = 15_000_000_000_000
+print(type(liczba))  # <class 'int'>
+print(liczba)  # 15000000000000
