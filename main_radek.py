36e08e4bc8f765475af585d3aa151a21f1563738
radoslawjaniak <rajkokret660@gmail.com>
2025-11-12 10:50:34 +0100

    [1 2] update main_radek.py

---
+++ main_radek.py
@@ -0,0 +1,8 @@
+print('Radek')
+# (.venv) radoslawjaniak@MacBook-Air-radosaw-2 PythonPodstawyLidl2_12_05_2025 % python main_radek.py
+# Radek
+# wheel, theme
+# 2 x shift wyszukiwarka
+# https://pep8.org/
+# snake_case
+# ctrl alt l - formatuje wg pep8
