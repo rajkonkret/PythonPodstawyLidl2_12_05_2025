e49294ff9bebcc175efc02be3b8de8eefeddff6c
radoslawjaniak <rajkokret660@gmail.com>
2025-11-13 10:51:28 +0100

    [2 6] add warunki.py

---
+++ day2/warunki.py
@@ -0,0 +1,17 @@
+# instrukcje warunkowe
+# instrukcje sterowanie przepływem programu
+# if -  w zależności od warunku (True/False) wykonuje odpowiedni blok programu
+
+odp = True
+# odp = False
+# Indend Rainbow
+if odp:
+    # wykona się gdy warunek True
+    print("Kod programu")
+    print("Kod programu")
+    print("Kod programu")
+    print("Kod programu")
+    print("Kod programu")
+
+print("Dalsza częśc programu")
+
