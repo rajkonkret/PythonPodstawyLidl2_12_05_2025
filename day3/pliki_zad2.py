3cf45abf2b51f76169659b46f505d801c0c309f4
radoslawjaniak <rajkokret660@gmail.com>
2025-11-14 09:26:52 +0100

    [3 1] add day3

---
+++ day3/pliki_zad2.py
@@ -0,0 +1,28 @@
+import chardet
+
+# print(chardet)
+
+file_path = 'test.log'
+
+# rb - wczytanie binarnir
+with open(file_path, "rb") as file:
+    raw_data = file.read()
+
+print(raw_data)
+
+result = chardet.detect(raw_data)
+print(result)
+# {'encoding': 'utf-8', 'confidence': 0.938125, 'language': ''}
+
+encoding = result['encoding']
+
+print(encoding) # utf-8utf-8
+print(raw_data.decode(encoding=encoding))
+
+# Powitanie2
+# Kolejny2
+# Jeszcze jedno2
+# Powitanie2
+# Kolejny2
+# Jeszcze jedno2
+# Dośążźdane
