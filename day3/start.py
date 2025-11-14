a8013b47341677de77056915253a218ee3cbf763
radoslawjaniak <rajkokret660@gmail.com>
2025-11-14 14:05:30 +0100

    [3 16] update fun7

---
+++ day3/start.py
@@ -0,0 +1,16 @@
+# import fun1
+
+# fun1.dodaj()
+import pakiet
+
+# print(pakiet.powitanie())
+
+from pakiet import fun
+
+fun.powitanie()  # Witam!!!
+
+import pakiet.fun as pk  # alias
+
+pk.powitanie()
+
+pakiet.info()
---
+++ pakiet/fun.py
@@ -0,0 +1,14 @@
+def powitanie():
+    print("Witam!!!")
+
+
+def info():
+    print("Jestem pakietem")
+
+
+if __name__ == '__main__':
+    info()
+    info()
+    info()
+    info()
+    info()
