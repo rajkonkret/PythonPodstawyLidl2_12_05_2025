1b2ed8a96a167442b8e897712d1ea0ad820c628b
radoslawjaniak <rajkokret660@gmail.com>
2025-11-14 15:28:57 +0100

    [3 21] update api_nbp.py

--- day3/api_nbp.py
+++ day3/api_nbp.py
@@ -1,2 +1,24 @@
+import requests
 
 url = "https://api.nbp.pl/api/exchangerates/rates/A/usd/"
+
+response = requests.get(url)
+print(response)
+# <Response [200]>
+# 2xx ok
+# 3xx przekierowania, warningi
+# 404 brajk zasobu, 400 Bad req
+# 500 Internal serwer error
+print(response.text)
+# {"table":"A","currency":"dolar amerykański","code":"USD",
+# "rates":[{"no":"221/A/NBP/2025","effectiveDate":"2025-11-14","mid":3.6376}]}
+data = response.json()
+print(data)
+print(type(data))  # <class 'dict'>
+# {'table': 'A',
+# 'currency': 'dolar amerykański',
+# 'code': 'USD',
+# 'rates': [{'no': '221/A/NBP/2025', 'effectiveDate': '2025-11-14', 'mid': 3.6376}]}
+print(data['rates'])
+# [{'no': '221/A/NBP/2025', 'effectiveDate': '2025-11-14', 'mid': 3.6376}]
+print(data['rates'][0]['mid'])  # 3.6376
