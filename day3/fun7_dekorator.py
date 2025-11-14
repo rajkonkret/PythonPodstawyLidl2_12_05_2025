93023dcb95bc337c5065e5ec883c13125d3cd3cb
radoslawjaniak <rajkokret660@gmail.com>
2025-11-14 13:37:53 +0100

    [3 15] update fun7

--- day3/fun7_dekorator.py
+++ day3/fun7_dekorator.py
@@ -1,5 +1,12 @@
 # funkcja dekorator
 # dodaje funkcjonalnosc do innej funkcji
+
+from colorama import init, Fore, Style
+
+# pip install colorama
+init(autoreset=True)
+
+
 def dekorator(func):
     def wrapper():
         print("Dekorujemy")
@@ -8,11 +15,25 @@ def dekorator(func):
     return wrapper  # zwracamy adres funkcji
 
 
+def color_decorator(func):
+    def wrapper():
+        result = func()
+        return Fore.RED + result + Style.RESET_ALL
+
+    return wrapper
+
+
 @dekorator
 def hello():
     print("witaj Świecie")
 
 
+@color_decorator
+def hello2():
+    return "Tekst"
+
+
 hello()
 # Dekorujemy
 # witaj Świecie
+print(hello2())  # Tekst
