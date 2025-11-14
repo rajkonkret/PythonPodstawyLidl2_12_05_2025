2174a2e2236c12a42c0166b15123eef636371b0e
radoslawjaniak <rajkokret660@gmail.com>
2025-11-14 15:11:52 +0100

    [3 18] update kl2

---
+++ day3/kl2.py
@@ -0,0 +1,60 @@
+from abc import ABC, abstractmethod
+
+
+class Ptak(ABC):
+    """
+    Klasa Ptaak  w pythonie
+    """
+
+    def __init__(self, gatunek, szybkosc):
+        """
+        Metoda inicjalizująca
+        :param gatunek:
+        :param szybkosc:
+        """
+        self.gatunek = gatunek
+        self.szybkosc = szybkosc
+
+    def latam(self):
+        print("Tu", self.gatunek, "Lecę z szybkościa", self.szybkosc)
+
+    # metoda abstrakcyjna
+    @abstractmethod
+    def wydaj_odglos(self):
+        pass
+
+
+class Kura(Ptak):
+    """
+    Dziedziczy po kalsie Ptak
+    """
+
+    def __init__(self, gatunek):
+        super().__init__(gatunek, 0)
+
+    def latam(self):
+        print("Tu", self.gatunek, 'Ja nie latam')
+
+    def wydaj_odglos(self):
+        print("Ko ko ko ko ")
+
+
+class Orzel(Ptak):
+
+    def wydaj_odglos(self):
+        print("Kier kir kier kir")
+
+
+# TypeError: Can't instantiate abstract class Ptak without an implementation for abstract method 'wydaj_odglos'
+# or1 = Ptak("Orzel", 54)
+# print(or1.szybkosc)
+# print(or1.gatunek)
+# # 54
+# # Orzel
+# or1.latam()  # Tu Orzel Lecę z szybkościa 54
+# kur1 = Ptak("Kura", 0)
+# kur1.latam()  # Tu Kura Lecę z szybkościa 0
+
+kur2 = Kura("Kura")
+kur2.latam()  # Tu Kura Ja nie latam
+kur2.wydaj_odglos()
